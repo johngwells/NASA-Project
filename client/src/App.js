70e8e98dd96ab5ecbfb83638aa4f124331ea1321
John Wells <molekewl2017@gmail.com>
2022-01-01 23:03:37 -0800

    create folder structure for the server NodeJS | Create react app for the client side & folder structure

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+/* Link: Launch
+  DropDown
+  Launch Date = Calender
+  Mission Name - ''
+  Rocket Type - ''
+  Destination Exoplanet
+  button Launch Mission
+*/
+
+/* link: Upcoming - Future Missions
+No. Date Mission Rocket Destination
+x deletes item 
+*/
+
+/* link: History - Previous Missions
+  No. Date Mission Rocket Customers
+*/
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
