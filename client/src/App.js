d21ca0f1db4225f04fdb2d53abee0cccd0c0f281
John Wells <molekewl2017@gmail.com>
2022-01-04 04:05:21 -0800

    React - breakdown into smaller components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-import Select from 'react-select';
+import DropDown from './components/DropDown';
 
 /* Link: Launch
   DropDown
@@ -36,60 +36,26 @@ function App() {
     fetchMyAPI();
   }, []);
 
-  const customStyles = {
-    option: (provided, state) => ({
-      ...provided,
-      backgroundColor: '#1DA1F2',
-      borderBottom: '1px dotted pink',
-      color: state.isSelected ? '#000' : 'white',
-      padding: 20,
-    }),
-    control: () => ({
-      // none of react-select's styles are passed to <Control />
-      width: 200,
-    }),
-    singleValue: (provided, state) => {
-      const opacity = state.isDisabled ? 0.5 : 1;
-      const transition = 'opacity 300ms';
-  
-      return { ...provided };
-    }
-  }
+  // const mappedData = planets.map(planet => {
+  //   return (
+  //     <ul key={planet['kepler_name']}>
+  //       <li>{planet['kepler_name']}</li>
+  //     </ul>
+  //   );
+  // });
 
-  const mappedData = planets.map(planet => {
-    return (
-      <ul key={planet['kepler_name']}>
-        <li>{planet['kepler_name']}</li>
-      </ul>
-    );
-  });
-
-  const dropDownOptions = [];
-
-  const dropdownData = planets.map(planet => {
-    let options = {
-      value: planet['kepler_name'],
-      label: planet['kepler_name']
-    };
-
-    return dropDownOptions.push(options);
-  });
-
-  const loadingList = isLoading ? 'loading...' : mappedData;
-  const loadingDropdown = isLoading ? 'loading' : dropdownData;
+  // const loadingList = isLoading ? 'loading...' : mappedData;
 
   return (
     <div className='App'>
       <header className='App-header'>
         <section>
           <h1>NASA Project</h1>
-          <div>
-            <p>Planets With Possible Life:</p>
-            {/* {loadingList} */}
-          </div>
+          <p>Planets With Possible Life:</p>
+          {/* {loadingList} */}
         </section>
         <section>
-          <Select styles={customStyles} options={dropDownOptions}></Select>
+          <DropDown planets={planets} />
         </section>
       </header>
     </div>
---
+++ client/src/components/DropDown.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Select from 'react-select';
+
+function DropDown({ planets }) {
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: '#1DA1F2',
+      borderBottom: '1px dotted white',
+      color: state.isSelected ? 'black' : 'white',
+      padding: 20
+    }),
+    control: () => ({
+      // none of react-select's styles are passed to <Control />
+      width: 200
+    }),
+    singleValue: (provided, state) => {
+      const opacity = state.isDisabled ? 0.5 : 1;
+      const transition = 'opacity 300ms';
+
+      return { ...provided, opacity };
+    }
+  };
+
+  const options = [];
+
+  planets.map(planet => {
+    const planetOptions = {
+      value: planet['kepler_name'],
+      label: planet['kepler_name']
+    };
+    return options.push(planetOptions);
+  });
+
+  return (
+    <>
+      <Select styles={customStyles} options={options}></Select>
+    </>
+  );
+}
+
+export default DropDown;
